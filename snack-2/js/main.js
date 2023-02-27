056251fbcfebb1ca900924a9562dde9b907d60e5
LeoSallu <L.sallustio99@gmail.com>
2023-02-27 11:54:05 +0100

    snack-2

---
+++ snack-2/js/main.js
@@ -0,0 +1,13 @@
+// JSsnack2
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+const numberInput = Number(prompt('Inserisci un numero'));
+
+if(numberInput % 2==0){
+   console.log(numberInput);
+   
+ }
+ else {
+    console.log(numberInput + 1);
+ }
