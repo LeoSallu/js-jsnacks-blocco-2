5926504a6422e4300715064f633d8170651f582e
LeoSallu <L.sallustio99@gmail.com>
2023-03-01 11:05:44 +0100

    1-snack prompt number

---
+++ snack-1/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+for(let i=0; i<=5; i++){
+    const input= Number(prompt('Inserisci un numero'));
+    console.log(input);
+};
+
+let inputNum = 0;
+
+while(inputNum < 5){
+    inputNum++;
+    const input = Number(prompt('Inserisci un numero')); 
+    console.log(input);
+    inputNum<5;
+}
